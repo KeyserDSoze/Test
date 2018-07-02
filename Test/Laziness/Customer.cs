f677dbb7ffd18a4514c3cc2205bcbf7cb3cec228
Alessandro Rapiti <a.rapiti@vetrya.com>
2018-07-03 00:44:33 +0200

    Add project files.

---
+++ Test/Laziness/Customer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Laziness
+{
+    public class Customer
+    {
+        public string Name { get; set; }
+        private Lazy<List<Order>> _Orders = new Lazy<List<Order>>(() => Order.GetOrders(null));
+        public List<Order> Orders { get { return this._Orders.Value; } }
+        public Customer(string _Name)
+        {
+            this.Name = _Name;
+        }
+    }
+    public class Order
+    {
+        public int X { get; set; }
+        public Order(int _X)
+        {
+            this.X = _X;
+        }
+        public static List<Order> GetOrders(string _ByName)
+        {
+            List<Order> orders = new List<Order>();
+            orders.Add(new Order(1));
+            orders.Add(new Order(2));
+            orders.Add(new Order(3));
+            Console.WriteLine("Get Orders");
+            return orders;
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Test.Laziness;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer c = new Customer("Test");
+            Console.WriteLine("Waiting to get values from Orders");
+            Console.ReadLine();
+            foreach(var x in c.Orders)
+            {
+                Console.WriteLine(x.X.ToString());
+            }
+            Console.ReadLine();
+        }
+    }
+}
