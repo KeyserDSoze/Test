0f71426855b032dae3ce023196911265a018c116
Alessandro Rapiti <a.rapiti@vetrya.com>
2018-07-26 00:18:29 +0200

    1.0.0-beta

---
+++ Test/CovarianceControvariance/Animal.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.CovarianceControvariance
+{
+    public interface ITypeA<in T> { }
+    public interface ITypeB<out T> { }
+    public class Animal
+    {
+        public int Type { get; set; }
+    }
+    public class Dog : Animal
+    {
+        public int NumberOfEyes { get; set; } = 2;
+    }
+    public class Cat : Animal
+    {
+        public int NumberOfMouth { get; set; } = 1;
+    }
+
+    public class Example3
+    {
+        public void Covariance()
+        {
+            ITypeA<Dog> typeA = GetAnimal();
+        }
+
+        static ITypeA<Animal> GetAnimal()
+        {
+            return null;
+        }
+
+        public void Contravariance()
+        {
+            ITypeB<Animal> typeB = GetDog();
+        }
+
+        static ITypeB<Dog> GetDog()
+        {
+            return null;
+        }
+    }
+}
---
+++ Test/CovarianceControvariance/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.CovarianceControvariance
+{
+    public class Person
+    {
+        public string Name { get; set; }
+    }
+
+    public class Teacher : Person
+    {
+        public string Surname { get; set; }
+    }
+
+    public class MailingList
+    {
+        public void Add(IEnumerable<Person> people) { }
+    }
+    public class Example2
+    {
+        public Example2()
+        {
+            MailingList mailingList = new MailingList();
+            List<Teacher> teach = new List<Teacher>();
+            List<Person> person = new List<Person>();
+            mailingList.Add(teach);
+            mailingList.Add(person);
+        }
+    }
+}
